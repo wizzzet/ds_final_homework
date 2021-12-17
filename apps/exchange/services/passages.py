1ff2e43e14c18616ae895cdcbd3d8b11f3700753
Raphael Kamashev <wizzzet@gmail.com>
2021-12-17 18:23:14 +0300

    jupyter - эксперименты на сервере с GPU

--- apps/exchange/services/passages.py
+++ apps/exchange/services/passages.py
@@ -3,6 +3,19 @@ import os
 
 from django.conf import settings
 from exchange import models
+from jinja2.filters import do_truncate
+
+from snippets.jinja2 import environment
+
+jinja2_env = environment()
+
+
+def truncate_passage(passage):
+    """
+    Обрезка последовательности до 512 (теоретический предел модели),
+    дабы сэкономить трафик и память
+    """
+    return do_truncate(jinja2_env, passage, 512, end='')
 
 
 def passages_generator(source_site=None):
@@ -23,8 +36,8 @@ def passages_generator(source_site=None):
             yield post.id, post.most_voted_answer_id, 3, post.title_cleaned
 
         if post.body_cleaned:
-            for sentence in post.body_cleaned.split('\n'):
-                yield post.id, post.most_voted_answer_id, 2, sentence
+            passage = truncate_passage(post.body_cleaned)
+            yield post.id, post.most_voted_answer_id, 2, passage
 
         answers = post.children.filter(
             source_type=2,
@@ -33,8 +46,8 @@ def passages_generator(source_site=None):
         ).exclude(body_cleaned='').only('id', 'body_cleaned')
 
         for answer in answers:
-            for sentence in answer.body_cleaned.split('\n'):
-                yield post.id, answer.id, 1, sentence
+            passage = truncate_passage(answer.body_cleaned)
+            yield post.id, answer.id, 1, passage
 
 
 def export_passages(file_path=None, source_site=None):
@@ -55,12 +68,13 @@ def export_passages(file_path=None, source_site=None):
             i += 1
 
             if i % 1000 == 0:
+                passage_print = passage.replace("\n", " ")
                 print(
                     f'{(str(i) + ")").ljust(12)}'
                     f'{str(post_id).ljust(12)}'
                     f'{str(answer_id).ljust(12)}'
                     f'{sentence_type}: '
-                    f'{passage}'
+                    f'{passage_print}'
                 )
 
             writer.writerow((post_id, answer_id, sentence_type, passage))
